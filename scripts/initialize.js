42d6d8f5cb6586a258b3724ade721f89a0ff71f5
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2020-09-17 17:04:39 +0200

    Title page
    Partially completed

---
+++ scripts/initialize.js
@@ -0,0 +1,5 @@
+function init() {
+    window.playerColors = []
+    document.getElementById("welcome-header").classList.toggle("faded")
+    document.getElementById("welcome-desc").classList.toggle("faded")
+}

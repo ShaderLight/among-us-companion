6349620be943547245a0c5f57b47e249feeea250
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2020-09-18 12:48:44 +0200

    Checkboxes

---
+++ scripts/color_management.js
@@ -0,0 +1,23 @@
+function toggleColor(color) {
+    var clickedImage = document.getElementById(color)
+    if (clickedImage.getAttribute('src') == `img/${color}_yes.png`) {
+        clickedImage.src = `img/${color}_no.png`
+        window.playerColors = removeOne(playerColors, color)
+    }
+    else {
+        clickedImage.src = `img/${color}_yes.png`
+        window.playerColors.push(color)
+    }
+}
+
+function removeOne(colorArray, color){
+    var output, len;
+    len = colorArray.length
+    for (i = 0; i < len; i++) {
+        if (colorArray[i] == color) {
+            delete colorArray[i]
+        }
+    }
+    output = colorArray.filter(function() { return true; });
+    return output
+}
--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,5 +1,13 @@
-function init() {
-    window.playerColors = []
+async function init() {
+    window.playerColors = ["red", "blue", "green", "pink", "orange", "yellow", "grey", "white", "purple", "brown", "cyan", "lime"]
     document.getElementById("welcome-header").classList.toggle("faded")
     document.getElementById("welcome-desc").classList.toggle("faded")
+    document.getElementById("begin-button").classList.toggle("faded")
+    await sleep(500)
+    document.getElementById("color-selection").classList.toggle("faded")
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
